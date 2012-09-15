a54fbcfef8365471c8e5d84fa98f6f9c6672a5be
crlsmtzprds <crlmtzprds@live.com.mx>
2012-09-15 01:30:06 -0500

    remove syntax error line 12
    There were a coma in line 12 *remove*

--- examples/script.js
+++ examples/script.js
@@ -9,7 +9,7 @@ var res = [8192, 4096, 2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1, 0.5]
 ,map = new L.Map('map', {
   crs: crs
   ,scale: scale,
-  ,continuousWorld: true
+  continuousWorld: true
 })
 ,mapUrl = 'http://api.geosition.com/tile/lmv/{z}/{x}/{y}.png'
 ,attrib = 'Map data &copy; 2011 Lantmäteriet, Imagery &copy; 2011 Kartena'
